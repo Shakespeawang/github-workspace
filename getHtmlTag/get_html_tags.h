492afa154796de8ca5231a99209095f0b28cc733
li-phone <li-phone@github.com>
2018-03-15 11:32:02 +0800

    4

---
+++ getHtmlTag/get_html_tags.h
@@ -0,0 +1,34 @@
+
+
+/*******************************************
+1)根据文件名读取文本文件，存储到string中
+2)从表格里提取一条信息
+3)添加到到csv文件中
+********************************************/
+
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <string>
+#include <Windows.h>
+#include <locale.h>  
+#include <comutil.h>  
+
+using namespace std;
+#pragma comment(lib, "comsuppw.lib")  
+
+#define _HTML			"HTML"
+
+
+bool readFile(string filename, string & html);
+bool writeFile(string filename, string record);
+
+bool isBlank(char ch);
+string clearBlank(string str);
+
+string getTag(string tag, string str, int * i);
+string getTagContent(string tagCode);
+string getTopContent(string tagCode);
+
+string getHtmlTag(const string html, const string tags, const string restricts, const string out_tags);
+
